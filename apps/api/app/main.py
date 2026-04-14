147ba2ae104e51e78bc15276746364634b60b7c4
A LEELA KRISHNA REDDY <aleelakrishnareddyar@gmail.com>
2026-04-14 23:03:25 +0530

    main.py created

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+origins = [ "http://localhost:3000", ]
+
+app.add_middleware(
+    CORSMiddleware,
+    alloe_origins=origins,
+    allow_credentials=True,
+    alloe_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.get("/")
 def root():
-    return {"message": "API running 🚀"}
+    return {"message": "Api is up and running"}
